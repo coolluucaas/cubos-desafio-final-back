6354eaaee4ffce1a6e01d35cac7e46b8575c8314
coolluucaas <lucaspsanta@gmail.com>
2021-10-02 11:39:45 -0300

    + controllers

---
+++ src/controllers/clientsController.js
@@ -0,0 +1,38 @@
+const knex = require('../config/databaseConextion')
+
+const cadastrarClientes = async (req, res) => {
+    const { email, ...dadosCliente } = req.body
+    const { id: usuario_id } = req.usuario
+
+    try {
+        const clienteCheck = await knex('clientes')
+            .where('email', email)
+            .first()
+
+        if (clienteCheck) {
+            return res
+                .status(400)
+                .json(
+                    'O email já foi cadastro, por favor insira um email diferente.'
+                )
+        }        
+
+        const clienteObj = {
+            usuario_id,           
+            email,           
+            ...dadosCliente,
+        }
+
+        const cliente = await knex('clientes').insert(clienteObj)
+
+        if (!cliente) {
+            return res.status(400).json('O cliente não foi cadastrado.')
+        }
+
+        return res.status(200).json('O cliente foi cadastrado com sucesso!')
+    } catch (error) {
+        return res.status(400).json(error.message)
+    }
+}
+
+module.exports = cadastrarClientes
---
+++ src/controllers/loginController.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const knex = require('../config/databaseConextion')
+
+const logarUsuario = async (req, res) => {
+    const { email, senha } = req.body
+
+    try {
+        const usuario = await knex('usuarios').where('email', email).first()
+
+        if (!usuario) {
+            return res.status(400).json('O usuario não foi encontrado')
+        }
+
+        const senhaVerificacao = await bcrypt.compare(senha, usuario.senha)
+
+        if (!senhaVerificacao) {
+            return res.status(400).json('Email e senha não confere')
+        }
+
+        const token = jwt.sign({ id: usuario.id }, process.env.JWT_SENHA, {
+            expiresIn: '8h',
+        })
+
+        const { senha: senhaUsuario, ...dadosUsuario } = usuario
+
+        return res.status(200).json({
+            usuario: dadosUsuario,
+            token,
+        })
+    } catch (error) {
+        return res.status(400).json(error.message)
+    }
+}
+
+module.exports = logarUsuario
