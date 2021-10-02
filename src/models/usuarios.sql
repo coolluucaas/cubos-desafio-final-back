f743015af68b7edf88a2cd9a4e0319a9ea70b0ca
coolluucaas <lucaspsanta@gmail.com>
2021-10-02 11:36:43 -0300

    renamed files

--- src/models/usuarios.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE usuarios (
-    id serial primary key ,
-    nome text not null,  
-    email text not null,
-    senha text not null,
-  	cpf varchar(14),
-    telefone varchar(15)
-  );
-  
