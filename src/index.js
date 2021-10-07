565471a22fc16409ab7c3d98884972b88585aa6f
coolluucaas <lucaspsanta@gmail.com>
2021-10-07 19:04:50 -0300

    reorganizacao do index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 require('dotenv').config({ path: `./env/.env.${process.env.NODE_ENV}` })
 const express = require('express')
+const cors = require('cors')
 
 const routesUsuarios = require('./routes/usersRoutes')
 const routesClientes = require('./routes/clientsRoutes')
-
-const cors = require('cors')
+const routesCobrancas = require('./routes/debtsRoutes')
 
 const app = express()
 
 app.use(cors())
-
 app.use(express.json())
 
 app.use(routesUsuarios)
 app.use(routesClientes)
+app.use(routesCobrancas)
 
 app.listen(process.env.PORT || 3003)
