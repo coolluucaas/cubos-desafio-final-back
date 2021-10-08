0754f470b9a041df3d94e9f16291c73530427bc3
coolluucaas <lucaspsanta@gmail.com>
2021-10-08 19:48:46 -0300

    +func cadastrarCobranca

--- src/controllers/debtsController.js
+++ src/controllers/debtsController.js
@@ -1,11 +1,12 @@
 const knex = require('../config/databaseConnection')
 
-const listarCobrancas= async (req, res) => {
+const listarCobrancas = async (req, res) => {
     try {
-        const cobrancas = await knex.select([
+        const cobrancas = await knex
+            .select([
                 'id as id_cobranca',
                 'nome',
-                'descricao',           
+                'descricao',
                 'valor',
                 knex.raw(`( 
                     CASE 
@@ -13,8 +14,9 @@ const listarCobrancas= async (req, res) => {
                     WHEN esta_pago IS FALSE AND data_vencimento< NOW() THEN 'PENDENTE'  
                     ELSE 'VENCIDO'
                     END
-                    ) as status`)              
-            ]).from('cobrancas')
+                    ) as status`),
+            ])
+            .from('cobrancas')
 
         return res.status(200).json(cobrancas)
     } catch (error) {
@@ -22,6 +24,23 @@ const listarCobrancas= async (req, res) => {
     }
 }
 
-module.exports = {    
-    listarCobrancas
+const cadastrarCobranca = async (req, res) => {
+    const cobrancaObj = req.body
+
+    try {        
+        const cobranca = await knex('cobrancas').insert(cobrancaObj)
+
+        if (!cobranca) {
+            return res.status(400).json('A cobranca não foi cadastrada.')
+        }
+
+        return res.status(200).json('A cobranca foi cadastrada com sucesso.')
+    } catch (error) {
+        return res.status(400).json(error.message)
+    }
+}
+
+module.exports = {
+    listarCobrancas,
+    cadastrarCobranca
 }
