54fd4537bf0b6a06b88e7ccc1f2206a0b42eaebf
coolluucaas <lucaspsanta@gmail.com>
2021-10-15 15:08:34 -0300

    identacao de código

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,21 +1,32 @@
 const express = require('express')
 const logarUsuario = require('../controllers/loginController')
-const { cadastrarUsuario, obterPerfil, editarPerfilUsuario } = require('../controllers/usersController')
+const {
+    cadastrarUsuario,
+    obterPerfil,
+    editarPerfilUsuario,
+} = require('../controllers/usersController')
 const verificadorLogin = require('../middlewares/authMiddleware')
 const validation = require('../middlewares/validationMiddleware')
 const schemaLogin = require('../validations/loginValidation')
 const schemaCadastrarUsuario = require('../validations/cadastrarUsuarioValidation')
 const schemaEditarPerfilUsuario = require('../validations/editarPerfilUsuarioValidation')
 
-
 const routesUsuarios = express()
 
-routesUsuarios.post('/',validation(schemaLogin), logarUsuario)
-routesUsuarios.post('/usuario',validation(schemaCadastrarUsuario), cadastrarUsuario)
+routesUsuarios.post('/', validation(schemaLogin), logarUsuario)
+routesUsuarios.post(
+    '/usuario',
+    validation(schemaCadastrarUsuario),
+    cadastrarUsuario
+)
 
 routesUsuarios.use(verificadorLogin)
 
 routesUsuarios.get('/usuario', obterPerfil)
-routesUsuarios.put('/usuario',validation(schemaEditarPerfilUsuario), editarPerfilUsuario)
+routesUsuarios.put(
+    '/usuario',
+    validation(schemaEditarPerfilUsuario),
+    editarPerfilUsuario
+)
 
 module.exports = routesUsuarios
